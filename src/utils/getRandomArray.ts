69590812993d9e54dd650270700dcc60e37d2d4e
saurabh <saurabhmehta1601@gmail.com>
2021-06-22 22:10:14 +0530

    initial setup

---
+++ src/utils/getRandomArray.ts
@@ -0,0 +1,9 @@
+// both endpoints a and b included
+export const  getRandomNumber = (a:number,b:number) : number => a + Math.floor(Math.random() * (b- a + 1))
+
+
+export const getRandomArray = (size:number , a:number , b : number) =>{
+    let arr = []
+    for(let i =0 ; i < size ; i++) arr.push(getRandomNumber(a,b))
+    return arr
+}
