f9e1d9227d9a4a2b6e81cec97617787a9ef068fa
saurabh <saurabhmehta1601@gmail.com>
2021-12-02 13:03:25 +0530

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,15 +3,44 @@ import styled from "styled-components"
 
 export default function Footer() {
     return ( <StyledFooter>
-        <p>Copyright &copy; {new Date().getFullYear()}</p>
+            <p id="credit">Made with  by  <a target="_blank" href="https://saurabhmehta.vercel.app">Saurabh</a></p>
+            <p id="contact"><a target="_blank" href="https://saurabhmehta.vercel.app">
+            See the source code on <i className="fab fa-github"></i>
+            </a></p>
     </StyledFooter>)
 }
 
 const StyledFooter = styled.footer`
     background-color: var(--clr-dark2) ;
-    word-spacing: 4px ; 
-    letter-spacing: 2px;
+    display:grid;
+    place-items:center ;
     color:#ddd;
-    text-align: center ;
     padding: 1em ;
+    font-family:sans-serif ;
+    p {
+        padding:0.3em ;
+        word-spacing: 4px ; 
+        letter-spacing: 2px;
+    }
+    a{
+        color:#eee ;
+        text-decoration:none ;
+    }
+    #credit {
+        font-size: 1.2rem ;
+
+        a {
+            color: #4ff766 ;
+        }
+    }
+    #contact {
+        font-size:0.97rem ;
+        opacity:0.6  ;
+        font-weight:900 ;
+
+        &:hover {
+            opacity: 1;
+            color: #54b2c7 ;
+        }
+    }
 `
