955409d61f477f8ea4d7c743af7388451af2aed0
Saurabh Mehta <saurabhmehta1601@gmail.com>
2021-12-15 17:27:00 +0530

    :white_check_mark: test(jest): add test for swap function in algorithms

---
+++ src/utils/__tests__/algos.test.js
@@ -0,0 +1,16 @@
+import {swap} from "../algos"
+
+it('changes array [1,2,3] to [3,2,1] by swap at position 0 and 2', () => {
+    const arr = [1,2,3]
+    swap(arr, 0,2)
+    expect(arr).toMatchObject([3,2,1])
+})
+
+it('does nothing if indices to be swapped are out of array bounds', () => {
+    const arr1 = [1,2,3,4,5]
+    const arr2 = [1,2,3,4,5]
+    swap(arr1 , 0 , 5)
+    swap(arr2 , -1, 2)
+    expect(arr1).toMatchObject([1,2,3,4,5])
+    expect(arr2).toMatchObject([1,2,3,4,5])
+})
--- src/utils/algos.ts
+++ src/utils/algos.ts
@@ -40,6 +40,8 @@ export const getAlgoByName = (algoName: string ) => {
 
  //function to swap two numbers in an array 
 export function swap(arr: number[], num1: number , num2 : number) {
+    if( num1 < 0 || num1 >= arr.length || num2 < 0 || num2 >= arr.length)
+        return 
     const temp = arr[num1];
     arr[num1] = arr[num2];
     arr[num2] = temp;
